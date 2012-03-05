b75826da9c5138d90c99975c18ced0061ea0e3d6
YN <ingar.ne@gmail.com>
2012-03-06 02:23:32 +0700

    web api

---
+++ AspNetWebApiHost/Controllers/TreeController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Http;
+using AspNetWebApiHost;
+using System.Net;
+using DAL;
+
+namespace AspNetWebApiHost.Controllers {
+    public class TreeController : ApiController {
+        private static Repository _repository = new Repository();
+
+        [HttpGet, ActionName("")]
+        public IEnumerable<Symptom> GetRootNodes()
+        {
+            return _repository.GetRootNodes();
+        }
+
+        [HttpGet]
+        public Symptom GetNode(string nodeId)
+        {
+            Int32 id;
+            if (!Int32.TryParse(nodeId, out id))
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            return _repository.GetNode(id);
+        }
+
+        [HttpGet, ActionName("children")]
+        public IEnumerable<Symptom> GetChildren(string nodeId)
+        {
+            Int32 id;
+            if (!Int32.TryParse(nodeId, out id))
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            return _repository.GetChildren(id);
+        }
+
+        [HttpPost]
+        public Symptom AddRoot(Symptom symptom)
+        {
+            if (symptom == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            return _repository.CreateNode(symptom.Name, null);
+        }
+
+        [HttpPost, ActionName("children")]
+        public Symptom AddChild(string nodeId, Symptom child)
+        {
+            Int32 id;
+            if (child == null || !Int32.TryParse(nodeId, out id))
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            var parent = _repository.GetNode(id);
+            return _repository.CreateNode(child.Name, parent);
+        }
+
+        [HttpDelete]
+        public void DeleteNode(string nodeId)
+        {
+            Int32 id;
+            if (!Int32.TryParse(nodeId, out id))
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            _repository.Delete(id);
+        }
+
+        [HttpGet, ActionName("diagnosis")]
+        public Diagnosis GetDiagnosis(string nodeId)
+        {
+            Int32 id;
+            if (!Int32.TryParse(nodeId, out id))
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            return _repository.GetDiagnosis(id);
+        }
+
+        [HttpPost, ActionName("diagnosis")]
+        public void SetDiagnosis(string nodeId, Diagnosis diagnosis)
+        {
+            Int32 id;
+            if (!Int32.TryParse(nodeId, out id))
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            _repository.SetDiagnosis(id, diagnosis);
+        }
+    }
+}
--- AspNetWebApiHost/Program.cs
+++ AspNetWebApiHost/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Web.Http.SelfHost;
 using System.Web.Http;
 using System.Web.Http.Routing;
+using System.Web.UI.WebControls;
 
 namespace AspNetWebApiHost {
     class Program {
@@ -12,15 +13,15 @@ namespace AspNetWebApiHost {
             var config = new HttpSelfHostConfiguration("http://localhost:8081/");
             config.Routes.MapHttpRoute(
                 name:"Default API",
-                routeTemplate: "api/{controller}/{department}/{id}",
-                defaults: new { department= RouteParameter.Optional, id = RouteParameter.Optional }
+                routeTemplate: "{controller}/{id}/{action}",
+                defaults: new { id = RouteParameter.Optional, action = RouteParameter.Optional }
             );
 
             HttpSelfHostServer server = new HttpSelfHostServer(config);
             server.OpenAsync().Wait();
 
             Console.ReadLine();
-            server.Dispose();
+            server.CloseAsync().Wait();
         
         }
     }
