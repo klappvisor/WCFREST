cd20b8f74a2707c93b6d0ad2483e23d5dedefc40
YN <klappvisor@gmail.com>
2012-03-06 15:29:58 +0700

    small improving

--- AspNetWebApiHost/Controllers/TreeController.cs
+++ AspNetWebApiHost/Controllers/TreeController.cs
@@ -6,93 +6,90 @@ using System.Web.Http;
 using AspNetWebApiHost;
 using System.Net;
 using DAL;
+using System.Net.Http;
 
 namespace AspNetWebApiHost.Controllers {
     public class TreeController : ApiController {
         private static Repository _repository = new Repository();
 
         [HttpGet, ActionName("index")]
-        public IEnumerable<Symptom> GetRootNodes()
-        {
-            
+        public IEnumerable<Symptom> GetRootNodes() {
+
             return _repository.GetRootNodes();
         }
 
         [HttpGet, ActionName("index")]
-        public Symptom GetNode(string nodeId)
-        {
+        public Symptom GetNode(string nodeId) {
             Int32 id;
-            if (!Int32.TryParse(nodeId, out id))
-            {
+            if (!Int32.TryParse(nodeId, out id)) {
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
             }
             return _repository.GetNode(id);
         }
 
         [HttpGet, ActionName("children")]
-        public IEnumerable<Symptom> GetChildren(string nodeId)
-        {
+        public IEnumerable<Symptom> GetChildren(string nodeId) {
             Int32 id;
-            if (!Int32.TryParse(nodeId, out id))
-            {
+            if (!Int32.TryParse(nodeId, out id)) {
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
             }
             return _repository.GetChildren(id);
         }
 
         [HttpPost]
-        public Symptom AddRoot(Symptom symptom)
-        {
-            if (symptom == null)
-            {
+        public HttpResponseMessage<Symptom> AddRoot(Symptom symptom) {
+            if (symptom == null) {
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
             }
-            return _repository.CreateNode(symptom.Name, null);
+
+            var newSymptom = _repository.CreateNode(symptom.Name, null);
+            return BuildCreateMessage(newSymptom);
         }
 
         [HttpPost, ActionName("children")]
-        public Symptom AddChild(string nodeId, Symptom child)
-        {
+        public HttpResponseMessage<Symptom> AddChild(string nodeId, Symptom child) {
             Int32 id;
-            if (child == null || !Int32.TryParse(nodeId, out id))
-            {
+            if (child == null || !Int32.TryParse(nodeId, out id)) {
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
             }
             var parent = _repository.GetNode(id);
-            return _repository.CreateNode(child.Name, parent);
+            var newSymptom = _repository.CreateNode(child.Name, parent);
+            return BuildCreateMessage(newSymptom);
         }
 
         [HttpDelete]
-        public void DeleteNode(string nodeId)
-        {
+        public void DeleteNode(string nodeId) {
             Int32 id;
-            if (!Int32.TryParse(nodeId, out id))
-            {
+            if (!Int32.TryParse(nodeId, out id)) {
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
             }
             _repository.Delete(id);
         }
 
         [HttpGet, ActionName("diagnosis")]
-        public Diagnosis GetDiagnosis(string nodeId)
-        {
+        public Diagnosis GetDiagnosis(string nodeId) {
             Int32 id;
-            if (!Int32.TryParse(nodeId, out id))
-            {
+            if (!Int32.TryParse(nodeId, out id)) {
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
             }
             return _repository.GetDiagnosis(id);
         }
 
         [HttpPost, ActionName("diagnosis")]
-        public void SetDiagnosis(string nodeId, Diagnosis diagnosis)
-        {
+        public void SetDiagnosis(string nodeId, Diagnosis diagnosis) {
             Int32 id;
-            if (!Int32.TryParse(nodeId, out id))
-            {
+            if (!Int32.TryParse(nodeId, out id)) {
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
             }
             _repository.SetDiagnosis(id, diagnosis);
+        }
+
+        private HttpResponseMessage<Symptom> BuildCreateMessage(Symptom s) {
+            var response = new HttpResponseMessage<Symptom>(s, HttpStatusCode.Created);
+            string relativeUri = Url.Route("DefaultAPI", new { nodeId = s.ID });
+            Uri baseUri = new Uri(String.Format("{0}://{1}", Request.RequestUri.Scheme, Request.RequestUri.Authority));
+            response.Headers.Location = new Uri(baseUri, relativeUri);
+            return response;
         }
     }
 }
